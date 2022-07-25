6b3576f4ecf517e14081f7d68e4baa10d7ec264c
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-07-25 14:10:34 +0430

    navbar for medium screen size added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
 twitter = document.querySelector(".twitter-logo");
-instagram = document.querySelector(".instagram-logo")
-telegram = document.querySelector(".telegram-logo")
+instagram = document.querySelector(".instagram-logo");
+telegram = document.querySelector(".telegram-logo");
 
-function hover(element){
-    element.setAttribute('src' , "F:/shopping site/Shopping-Site/assets/images/" + element.className + "-gif.gif")
+function hover(element) {
+  element.setAttribute(
+    "src",
+    "F:/shopping site/Shopping-Site/assets/images/" +
+      element.className +
+      "-gif.gif"
+  );
 }
 
-function unhover(element){
-    element.setAttribute('src' , "F:/shopping site/Shopping-Site/assets/images/" + element.className + ".png")
+function unhover(element) {
+  element.setAttribute(
+    "src",
+    "F:/shopping site/Shopping-Site/assets/images/" + element.className + ".png"
+  );
 }
+
+let navbar = document.querySelector("#navbar");
+let toggle = document.querySelector(".toggle");
+
+toggle.onclick = function () {
+  navbar.classList.toggle("active");
+  toggle.classList.toggle("active")
+};
